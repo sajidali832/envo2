4960b12e50a9545ba10daf587d5e0ddf0ac45d31
FASTO <fasto.org@gmail.com>
2025-07-28 08:32:49 +0000

    screen shot is not visible fix this

--- next.config.ts
+++ next.config.ts
@@ -16,6 +16,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'erfxugkthvipzzwguxzd.supabase.co',
+        port: '',
+        pathname: '/**',
+      }
     ],
   },
 };
